2810499300ffef051e7344a2d16847d46df629b6
Akash Kumar Mishra <akash.k.mishra03@gmail.com>
2025-02-01 21:40:23 +0530

    Add files via upload
    Finding palindrome number in java

---
+++ Palindrome.java
@@ -0,0 +1,17 @@
+class PalindromeNumber {
+    static boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+        int rev = 0;
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return x == rev || x == rev / 10;
+    }
+
+    public static void main(String[] args) {
+        int x = 121;
+        System.out.println(isPalindrome(x));
+    }
+}
